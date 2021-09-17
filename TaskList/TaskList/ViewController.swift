01af96c56c5deb0f9164d6526d4da632dc6db99c
Kathrine Kashtanova <e.kashtanova@mobile.agima.ru>
2021-09-17 23:08:04 +0400

    first commit

---
+++ TaskList/TaskList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TaskList
+//
+//  Created by Catherine Kashtanova on 17.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
